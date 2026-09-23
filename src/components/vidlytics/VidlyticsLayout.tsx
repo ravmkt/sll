caab92ae549b5b530cb9cf86838ba3a49b412083
SeuNome <seu@email.com>
2026-09-23 15:03:48 -0300

    refactor(vidlytics): consolida BibliotecaTab exclusivamente na pasta tabs e remove duplicatas de components

--- src/components/vidlytics/VidlyticsLayout.tsx
+++ src/components/vidlytics/VidlyticsLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import BibliotecaTab from '../components/vidlytics/BibliotecaTab';
+import BibliotecaTab from '../../pages/vidlytics/tabs/BibliotecaTab';
 import { 
   LayoutDashboard, 
   BarChart3, 
